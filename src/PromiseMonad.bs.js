b2bac61675039970aaefc1ce56392c4dd22a2605
Digitake <digitake@gmail.com>
2018-05-02 19:45:21 +0700

    Initial commit

---
+++ src/PromiseMonad.bs.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Curry = require("bs-platform/lib/js/curry.js");
+
+function defer(f) {
+  return new Promise((function (resolve, _) {
+                Curry._1(f, resolve);
+                return /* () */0;
+              }));
+}
+
+function $$return(a) {
+  return Promise.resolve(a);
+}
+
+function error(a) {
+  return Promise.reject(a);
+}
+
+function $great$great$eq(m, f) {
+  return m.then(Curry.__1(f));
+}
+
+exports.defer = defer;
+exports.$$return = $$return;
+exports.error = error;
+exports.$great$great$eq = $great$great$eq;
+/* No side effect */
